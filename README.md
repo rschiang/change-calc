1c213dd7eca22a54f74ac741ef15e017422fa97e
Poren Chiang <ren.chiang@gmail.com>
2015-03-05 04:24:30 +0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,10 @@
+ChangeCalc
+==========
+
+Calculate minimum needed changes.
+
+Useful when distributing large scales of varied cash.
+
+Type `END` to sum up all changes.
+
+Licensed under MIT License.
---
+++ calc.py
@@ -0,0 +1,36 @@
+s = ''
+
+units = [1000, 500, 100, 50, 10, 5, 1]
+total = 0
+total_changes = { unit : 0 for unit in units }
+
+while s.lower() != 'end':
+    try:
+        num = int(s.strip())
+    except ValueError:
+        s = input('>>> ')
+        continue
+
+    changes = {}
+    remaining = num
+    for unit in units:
+        change = remaining // unit
+        remaining = remaining % unit
+
+        if change > 0:
+            changes[unit] = change
+
+        if remaining == 0:
+            break
+
+    print(num, '=', ' + '.join(['{0} x {1}'.format(unit, changes[unit]) for unit in units if unit in changes]))
+
+    total += num
+    for unit, change in changes.items():
+        total_changes[unit] += change
+
+    s = input('>>> ')
+
+print('Total:', total)
+for unit in units:
+    print(unit, 'x', total_changes[unit])
